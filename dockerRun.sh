6aa57720f2f899c753216780728c77c17ee6e1d5
Antonio Lain <antlai@cafjs.com>
2016-02-28 21:45:13 -0800

    Support node 4.3

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #dockerRun <foo-device1> [</home/something/mytoken>] where the second arg is 
 #  optional if token already copied.
@@ -14,4 +14,4 @@ fi
 
 # need /dev/initctl to be able to shutdown the host
 # To really stop it docker rm -f <containerID>, otherwise restarts after boot
-docker run -d  -v /dev/initctl:/dev/initctl --device=/dev/i2c-1 --name=root-rpidaemon --restart=always -e MY_ID=$1 --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/bin/docker -v /control:/control -v /config:/config registry.cafjs.com:32000/root-rpidaemon
+docker run -d  -v /dev/initctl:/dev/initctl --device=/dev/i2c-1 --name=root-rpidaemon --restart=always -e MY_ID=$1 --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/bin/docker -v /control:/control -v /config:/config -e CONTROL_VOLUME=/control -e CONFIG_VOLUME=/config registry.cafjs.com:32000/root-rpidaemon
