10cf27cab190192d89d872eca5fb9234211518bc
Antonio Lain <antlai@cafjs.com>
2016-03-02 23:33:57 -0800

    Add shutdown.sh exec for Alpine 2

--- shutdown.sh
+++ shutdown.sh
@@ -2,5 +2,7 @@
 echo 1 > /proc/sys/kernel/sysrq
 #sync, unmount, and shutdown (still not completely safe because processes running)
 echo s > /proc/sysrq-trigger
+/bin/sleep 1
 echo u > /proc/sysrq-trigger
+/bin/sleep 1
 echo o  > /proc/sysrq-trigger
