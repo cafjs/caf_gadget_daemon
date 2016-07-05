32dd00907642dec73ea37783a1d80bfcc99d5fad
Antonio Lain <antlai@cafjs.com>
2016-07-05 01:00:49 -0700

    Add metadata support

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#dockerRun <foo-device1> [</home/something/mytoken>] where the second arg is 
+#dockerRun <foo-device1> [</home/something/mytoken>] where the second arg is
 #  optional if token already copied.
 
 
@@ -9,9 +9,9 @@ mkdir -p /config
 
 if [ $# -eq 2 ]
 then
-    cp $2 /config/token 
+    cp $2 /config/token
 fi
 
 # need /dev/initctl to be able to shutdown the host
 # To really stop it docker rm -f <containerID>, otherwise restarts after boot
-docker run -d  -v /dev/initctl:/dev/initctl --device=/dev/i2c-1 --name=root-rpidaemon --restart=always -e MY_ID=$1 --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/bin/docker -v /control:/control -v /config:/config -e CONTROL_VOLUME=/control -e CONFIG_VOLUME=/config registry.cafjs.com:32000/root-rpidaemon
+docker run -d  -v /dev/initctl:/dev/initctl --device=/dev/i2c-1 --name=root-rpidaemon --restart=always -e MY_ID=$1 --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /control:/control -v /config:/config -e CONTROL_VOLUME=/control -e CONFIG_VOLUME=/config registry.cafjs.com:32000/root-rpidaemon
