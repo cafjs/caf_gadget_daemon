d3ffa08e99a367fade7c720282fbb2b24828cb60
Antonio Lain <antlai@cafjs.com>
2018-12-29 12:17:16 -0800

    Support Node 10.X

--- dockerRun.sh
+++ dockerRun.sh
@@ -12,4 +12,4 @@ then
 fi
 
 # To really stop it docker rm -f <containerID>, otherwise restarts after boot
-docker run -d --name=root-rpidaemon --restart=always -e MY_ID=$1 -v /var/run/docker.sock:/var/run/docker.sock  -v /config:/config  -e CONFIG_VOLUME=/config registry.cafjs.com:32000/root-rpidaemon
+docker run -d --name=root-rpidaemon --restart=always -e MY_ID=$1 -v /var/run/docker.sock:/var/run/docker.sock  -v /config:/config  -e CONFIG_VOLUME=/config gcr.io/cafjs-k8/root-rpidaemon
